b366d94ce23b322b0d7c9750e6ecd9b6386f7b49
Gabi Dobocan <gabi@telepat.io>
2026-04-30 12:48:49 +0300

    feat: add new icon images for extension in various sizes
    - Added 16x16 pixel icon (16.png)
    - Added 32x32 pixel icon (32.png)
    - Added 48x48 pixel icon (48.png)
    
    These icons will be used for the extension's user interface.

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -26,5 +26,11 @@ export default defineConfig({
       default_popup: 'popup.html',
     },
     options_page: 'options.html',
+    icons: {
+      16: 'icon/16.png',
+      32: 'icon/32.png',
+      48: 'icon/48.png',
+      128: 'icon/128.png',
+    },
   },
 });
